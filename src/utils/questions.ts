688d1ac700461185282ec684509b401bae5a89ee
jhonatan lezcano <jhonatanlezcano.plv@gmail.com>
2022-08-16 20:52:46 -0500

    :heavy_plus_sign: add styled-components

---
+++ src/utils/questions.ts
@@ -0,0 +1,29 @@
+export const quetions = [
+  {
+    quetion: "¿Ventaja para quien?",
+    options: [
+      { text: "Usuarios y Finanzas", isCorrect: false },
+      { text: "Usuarios y Ciudadano", isCorrect: false },
+      { text: "Usuarios y Empresas", isCorrect: false },
+      { text: "Usuarios y Medioambientales", isCorrect: true },
+    ],
+  },
+  {
+    quetion: "¿Cual NO hace parte del concepto de la 7R's?",
+    options: [
+      { text: "Remover", isCorrect: true },
+      { text: "Rediseñar", isCorrect: false },
+      { text: "Reducir", isCorrect: false },
+      { text: "Recuperar", isCorrect: false },
+    ],
+  },
+  {
+    quetion: "¿Pregunta random?",
+    options: [
+      { text: "mala", isCorrect: false },
+      { text: "mala", isCorrect: false },
+      { text: "buena", isCorrect: true },
+      { text: "mala", isCorrect: false },
+    ],
+  },
+];
